c483283117425446e33a43c5a3a62e8520c66358
Ricardo Facco Pigatto <arpfacco@gmail.com>
2025-09-17 01:53:31 +0000

    add gera palavra

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import System.Random (randomRIO)
+import Control.Monad.IO.Class (liftIO)
+import Data.Text.Lazy (Text)
+
+
+palavras :: [Text]
+palavras = ["carta","pedra","plano","verde","festa","piano","prato","livro","filho",
+            "vento","tigre","canto","noite","falar","amigo","nuvem","risos","corpo", 
+            "casal","trigo"]
+
+
+getRandomPalavra :: IO Text
+getRandomPalavra = do
+    index <- randomRIO (0, length palavras - 1)
+    return $ palavras !! index
 
 main :: IO ()
 main = scotty 3000 $ do
-
-  -- Define your routes and handlers here
-  get "/" $ do
-    text "Hello, Haskell Web Service!"
+    middleware logStdoutDev
+    get "/" $ do
+        randomPalavra <- liftIO getRandomPalavra  
+        text randomPalavra
