f28e7048a27f2c8d8877146aa0b411a12166ba44
Ricardo Facco Pigatto <arpfacco@gmail.com>
2025-09-17 23:49:50 +0000

    add funcoes de dicas, checar tentativas

--- app/testes.hs
+++ app/testes.hs
@@ -6,32 +6,47 @@ import System.Random (randomRIO)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.IO as TIO
+import Data.IORef
 
-
+-- carregar palavras do .txt
 carregarPalavras :: IO [T.Text]
 carregarPalavras = do
     conteudo <- TIO.readFile "palavras.txt"
     return (T.lines conteudo)
 
-
+-- seleciona uma palavra aleatoria
 getRandomPalavra :: [T.Text] -> IO T.Text
 getRandomPalavra lista = do
     index <- randomRIO (0, length lista - 1)
     return $ lista !! index
 
-
+-- compara palpite com resposta
 comparaPalavra :: T.Text -> T.Text -> Bool
 comparaPalavra tentativa resposta = tentativa == resposta
 
+-- gera dicas a partir de um palpite
+gerarDicas :: T.Text -> T.Text -> (T.Text, T.Text, T.Text)
+gerarDicas tentativa resposta =
+    let certas   = [c | (c,r) <- T.zip tentativa resposta, c == r]
+        erradas  = [c | c <- T.unpack tentativa, c `elem` T.unpack resposta, c `notElem` certas]
+        ausentes = [c | c <- T.unpack tentativa, c `notElem` T.unpack resposta]
+    in (T.pack certas, T.pack erradas, T.pack ausentes)
 
-receberPalavra :: [T.Text] -> T.Text -> IO T.Text
-receberPalavra lista tentativa
-    | T.length tentativa /= 5 = return "A palavra precisa ter 5 letras!"
-    | otherwise = do
-        resposta <- getRandomPalavra lista
-        if comparaPalavra tentativa resposta
-           then return "Acertou!"
-           else return "Errou!"
+-- controla limite de tentativas
+checarTentativas :: Int -> Bool
+checarTentativas n = n < 6
+
+-- recebe um palpite e retorna mensagem com dicas
+receberPalpite :: T.Text -> T.Text -> Int -> T.Text
+receberPalpite tentativa resposta tentativas
+    | T.length tentativa /= 5 = "A palavra precisa ter 5 letras!"
+    | comparaPalavra tentativa resposta = "Acertou!"
+    | tentativas >= 5 = "Errou! A resposta era: " <> resposta
+    | otherwise =
+        let (certas, erradas, ausentes) = gerarDicas tentativa resposta
+        in "Letras em posicao certa: " <> certas
+        <> " | Letras em posicao errada: " <> erradas
+        <> " | Letras ausentes: " <> ausentes
 
 
 
