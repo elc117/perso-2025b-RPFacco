aa78fdd8587ce5df8bc90e0ad1c3d88ca950cde7
Ricardo Facco Pigatto <arpfacco@gmail.com>
2025-09-17 15:07:29 +0000

    add gera palavra

--- app/Main.hs
+++ app/Main.hs
@@ -8,15 +8,17 @@ import Data.Text.Lazy (Text)
 
 
 palavras :: [Text]
-palavras = ["carta","pedra","plano","verde","festa","piano","prato","livro","filho",
-            "vento","tigre","canto","noite","falar","amigo","nuvem","risos","corpo", 
-            "casal","trigo"]
+palavras = ["carta","pedra","plano","verde","festa","piano","prato","livro","filho","vento","tigre","canto","noite","falar","amigo","nuvem","risos","corpo","casal","trigo",
+            "couro","faixa","grato","poder","rolar","saber","terra","luzir","manha","morro","banho","cesta","campo","pente","moeda","rosto","sonho","dente","certo","olhos",
+            "pular","cedro","pedal","trevo","tanto","folha","magro","doido","pouco","beijo","forca","sorte","bolsa","calor","navio","cabra","prego","haste","claro","tempo"]
 
 
 getRandomPalavra :: IO Text
 getRandomPalavra = do
     index <- randomRIO (0, length palavras - 1)
     return $ palavras !! index
+
+
 
 main :: IO ()
 main = scotty 3000 $ do
