5cba910d86da467a4d9bd28ecd92806c324bdcc1
Ricardo Facco Pigatto <arpfacco@gmail.com>
2025-09-18 00:39:09 +0000

    troca de nome "tentativa" por "palpite"

--- app/Termo.hs
+++ app/Termo.hs
@@ -22,49 +22,51 @@ getRandomPalavra lista = do
 
 -- compara palpite com resposta
 comparaPalavra :: T.Text -> T.Text -> Bool
-comparaPalavra tentativa resposta = tentativa == resposta
+comparaPalavra palpite resposta = palpite == resposta
 
 -- gera dicas a partir de um palpite
 gerarDicas :: T.Text -> T.Text -> (T.Text, T.Text, T.Text)
-gerarDicas tentativa resposta =
-    let certas   = [c | (c,r) <- T.zip tentativa resposta, c == r]
-        erradas  = [c | c <- T.unpack tentativa, c `elem` T.unpack resposta, c `notElem` certas]
-        ausentes = [c | c <- T.unpack tentativa, c `notElem` T.unpack resposta]
+gerarDicas palpite resposta =
+        -- cria pares e compara
+    let certas   = [c | (c,r) <- T.zip palpite resposta, c == r]
+        -- letras que aparecem na resposta, mas nao estao em 'certas'
+        erradas  = [c | c <- T.unpack palpite, c `elem` T.unpack resposta, c `notElem` certas]
+        -- letras que nao estao na resposta
+        ausentes = [c | c <- T.unpack palpite, c `notElem` T.unpack resposta]
     in (T.pack certas, T.pack erradas, T.pack ausentes)
 
 -- controla limite de tentativas
 checarTentativas :: Int -> Bool
-checarTentativas n = n < 6
+checarTentativas numTentativa = numTentativa < 6
 
 -- recebe um palpite e retorna mensagem com dicas
 receberPalpite :: T.Text -> T.Text -> Int -> T.Text
-receberPalpite tentativa resposta tentativas
-    | T.length tentativa /= 5 = "A palavra precisa ter 5 letras!"
-    | comparaPalavra tentativa resposta = "Acertou!"
+receberPalpite palpite resposta numTentativa
+    | T.length palpite /= 5 = "A palavra precisa ter 5 letras!"
+    | comparaPalavra palpite resposta = "Acertou!"
     | otherwise =
-        let (certas, erradas, ausentes) = gerarDicas tentativa resposta
+        let (certas, erradas, ausentes) = gerarDicas palpite resposta
         in "Letras em posicao certa: " <> certas
         <> " | Letras em posicao errada: " <> erradas
         <> " | Letras ausentes: " <> ausentes
 
-
+-- começar o jogo
 jogar :: T.Text -> Int -> IO ()
-jogar resposta tentativa
-    | tentativa >= 6 = putStrLn $ "Você perdeu! A resposta era: " ++ T.unpack resposta
+jogar resposta numTentativa
+    | numTentativa >= 6 = putStrLn $ "Você perdeu! A resposta era: " ++ T.unpack resposta
     | otherwise = do
         putStrLn "Digite uma palavra de 5 letras:"
-        palpite <- getLine
-        if length palpite /= 5
+        entrada <- getLine
+        if length entrada /= 5
            then do
                putStrLn "A palavra precisa ter 5 letras! Tente novamente."
-               jogar resposta tentativa  
+               jogar resposta numTentativa  -- não incrementa
            else do
-               let resultado = receberPalpite (T.pack palpite) resposta tentativa
+               let resultado = receberPalpite (T.pack entrada) resposta numTentativa
                putStrLn (T.unpack resultado)
                if resultado == "Acertou!"
                   then putStrLn "Parabéns!"
-                  else jogar resposta (tentativa + 1) 
-
+                  else jogar resposta (numTentativa + 1)
 
 
 
